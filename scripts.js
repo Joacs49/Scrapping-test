e2e9ddd5ac934a21030275bd12c2d75cda5c24ca
joacs <gaelmzl49@gmail.com>
2025-05-03 00:49:57 -0500

    Prueba de Pagina para realizar Scrapping y envio de resultados al WhatsApp

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function activarScraping() {
+    fetch(window.config.webhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then((res) => {
+        const msg = document.getElementById("mensaje");
+        if (res.ok) {
+          msg.textContent = "✔ Automatización iniciada correctamente.";
+        } else {
+          msg.textContent = "✖ Error al iniciar la automatización.";
+          msg.style.color = "red";
+        }
+      })
+      .catch((error) => {
+        document.getElementById("mensaje").textContent =
+          "✖ Error en la conexión.";
+        console.error(error);
+      });
+  }
+  
